3df475385d7751ab90f728b174fc7e2f43d5f686
Marvins <murimi65@gmial.com>
2019-09-02 10:59:10 +0300

    changes

---
+++ app/src/main/java/com/example/submann/urls.java
@@ -0,0 +1,9 @@
+package com.example.submann;
+
+public class urls {
+    private static final String URL_ROOT = "http://10.0.2.2/api.php?apicall=";
+    public static final String URL_REGISTER = URL_ROOT + "register";
+    public static final String URL_LOGIN= URL_ROOT + "login";
+    public static final String URL_SAVE= URL_ROOT +"save";
+
+}
